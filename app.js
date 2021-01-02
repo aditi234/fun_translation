0dc33c0620f2e3238137c02b5d5f903879794597
Aditi Sahu <aditisahu234@gmail.com>
2021-01-02 11:48:48 +0530

    Translation

---
+++ app.js
@@ -0,0 +1,54 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+//var serverURL="https://api.funtranslations.com/translate/yoda.json"
+
+function getCharacter(){
+    var element=document.getElementById("choose").value;
+    if(element==1)
+    {
+        console.log("Minions");
+        serverURL="https://api.funtranslations.com/translate/minion.json";
+    }
+    else if(element==2)
+    {
+        console.log("Yoda");
+        serverURL="https://api.funtranslations.com/translate/yoda.json";
+    }
+    else
+    {
+        console.log("Groot");
+        serverURL="https://api.funtranslations.com/translate/groot.json"
+    }
+}
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+
+function clickHandler() {
+    var inputText = txtInput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
+};
+
+
+
+btnTranslate.addEventListener("click", clickHandler)
